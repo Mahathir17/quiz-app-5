23ba3004688bb492ebfb9d8b0bb56dfa4120b498
Mahathir Sadman <mahathirsadman17@gmail.com>
2022-08-03 18:25:36 +0600

    first commit

---
+++ js/question.js
@@ -0,0 +1,170 @@
+let questions = [
+    // {
+    //     numb:1,
+    //     question:'What Does HTML Stand For?',
+    //     answer:'Hyper Text Markup Language',
+    //     options:[
+    //         "Hyper Text Preprocessor",
+    //         "Hyper Text Markup Language",
+    //         "Hyper Text Multiple Language",
+    //         "Hyper Tool Multi Language"
+    //     ]
+    // },
+    // {
+    //     numb: 2,
+    //     question: "What Does CSS Stand For?",
+    //     answer: "Cascading Style Sheet",
+    //     options: [
+    //       "Common Style Sheet",
+    //       "Colorful Style Sheet",
+    //       "Computer Style Sheet",
+    //       "Cascading Style Sheet"
+    //     ]
+    //   },
+    //   {
+    //     numb: 3,
+    //     question: "What Does PHP Stand For?",
+    //     answer: "Hypertext Preprocessor",
+    //     options: [
+    //       "Hypertext Preprocessor",
+    //       "Hypertext Programming",
+    //       "Hypertext Preprogramming",
+    //       "Hometext Preprocessor"
+    //     ]
+    //   },
+    
+    //   {
+    //     numb: 4,
+    //     question: "What does SQL stand for?",
+    //     answer: "Structured Query Language",
+    //     options: [
+    //       "Stylish Question Language",
+    //       "Stylesheet Query Language",
+    //       "Statement Question Language",
+    //       "Structured Query Language"
+    //     ]
+    //   },
+
+    //   {
+    //     numb: 5,
+    //     question: "When did Bangladesh gain its independence?",
+    //     answer: "26 March 1971",
+    //     options: [
+    //       "25 March 1971",
+    //       "16 December, 1971",
+    //       "26 March 1971",
+    //       "None of them"
+    //     ]
+    //   },
+    //   {
+    //     numb: 6,
+    //     question: "What does XML stand for?",
+    //     answer: "eXtensible Markup Language",
+    //     options: [
+    //       "eXtensible Markup Language",
+    //       "eXecutable Multiple Language",
+    //       "eXTra Multi-Program Language",
+    //       "eXamine Multiple Language"
+    //     ]
+    //   },
+      {
+        numb: 1,
+        question: "Which one of the following is not an official language of the United Nations?",
+        answer: "Portuguese",
+        options: [
+          "Arabic",
+          "Chinese",
+          "Portuguese",
+          "Spanish"
+        ]
+      },
+      {
+        numb: 2,
+        question: " On July….., man walked on the Moon for the first time.",
+        answer: "20th",
+        options: [
+          "20th",
+          "21th",
+          "19th",
+          "18th"
+        ]
+      },
+      {
+        numb: 3,
+        question: "Which planet is known as the 'Watery Planet' ?",
+        answer: "Earth",
+        options: [
+          "Mercury",
+          "Earth",
+          "Mars",
+          "Jupiter"
+        ]
+      },
+      {
+        numb: 4,
+        question: " Halley's comet appears once in a period of ________",
+        answer: "76 years",
+        options: [
+          "24 years",
+          "32 years",
+          "76 years",
+          "84 years"
+        ]
+      },
+      // {
+      //   numb: 11,
+      //   question: "Which protocol provides e-mail facility among different hosts?",
+      //   answer: "SMTP",
+      //   options: [
+      //     "FTP",
+      //     "SMTP",
+      //     "TELNET",
+      //     "SNMP"
+      //   ]
+      // },
+      {
+        numb: 5,
+        question: "Fifth generation computers are based on",
+        answer: "Artificial Intelligence",
+        options: [
+          "Artificial Intelligence",
+          "Programming Intelligence",
+          "System Knowledge",
+          "VVLSI"
+        ]
+      },
+      // {
+      //   numb: 13,
+      //   question: "If the displayed system time and date is wrong, you can reset it using",
+      //   answer: "Control panel",
+      //   options: [
+      //     "Write",
+      //     "Calendar",
+      //     "Write file",
+      //     "Control panel"
+      //   ]
+      // },
+      // {
+      //   numb: 14,
+      //   question: "A Microsoft Windows is a(n)",
+      //   answer: "Operating system",
+      //   options: [
+      //     "Operating system",
+      //     "Graphic program",
+      //     "Word Processing",
+      //     "Database program"
+      //   ]
+      // },
+      // {
+      //   numb: 15,
+      //   question: "ATM means",
+      //   answer: "Automated Teller Machine",
+      //   options: [
+      //     "Automatic Teller Machine",
+      //     "All Time Money",
+      //     "Automated Teller Machine",
+      //     "Any Time Money"
+      //   ]
+      // },
+      
+]
